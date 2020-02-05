414bb22741c87f47ec9966135d71eab0810cbc8d
Calysto Chastain <10798368@my.uvu.edu>
2020-02-05 00:19:50 -0700

    character controller setup, gray box integration

---
+++ Reflex_Game/Assets/Scripts/FPS_Camera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class FPS_Camera : MonoBehaviour
+{
+    private Vector3 _position;
+    
+    public float orientSpeed;
+    
+    private float _rotationX = 0F;
+    private float _orientationX, _orientationY;
+
+    public Transform playerBody;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    private void Update()
+    {
+        _orientationX = Input.GetAxis("Mouse X") * orientSpeed * Time.deltaTime;
+        _orientationY = Input.GetAxis("Mouse Y") * orientSpeed * Time.deltaTime;
+
+        _rotationX -= _orientationY;
+        _rotationX = Mathf.Clamp(_rotationX, -90F, 90F);
+        
+        transform.localRotation = Quaternion.Euler(_rotationX, 0F, 0F);
+        playerBody.Rotate(Vector3.up * _orientationX);
+    }
+}
---
+++ Reflex_Game/Assets/Scripts/Player_Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player_Move : MonoBehaviour
+{
+   private Vector3 _position;
+   public float xSpeed, zSpeed, gravity;
+   private CharacterController _controller;
+
+   private void Start()
+   {
+      _controller = GetComponent<CharacterController>();
+   }
+
+   private void Update()
+   {
+      _position.Set(Input.GetAxis("Horizontal") * xSpeed, -gravity * Time.deltaTime, Input.GetAxis("Vertical") * zSpeed);
+      
+      _position = _controller.transform.TransformDirection(_position);
+      _controller.Move(_position * Time.deltaTime);
+   }
+}
